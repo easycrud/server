33ce0fe63321081ba7e3e0d18c2f6b3b0b8547a7
YANG Qia <2013xile@gmail.com>
2022-07-06 14:58:08 +0800

    add db connect module && change .d.ts file

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,23 @@
+import * as Toolkits from '@easycrud/toolkits';
+import { Knex } from 'knex';
+import * as Router from 'koa-router';
+
 declare namespace crud {
-    interface opts {
-        
+    interface DBConfig extends Knex.Config {
+        database?: string;
+    }
+
+
+    interface Options {
+
+        path: string;
+
+        tables: Toolkits.TableDefinition;
+
+        dbConfigs: DBConfig[];
     }
 }
+
+declare function crud(opts: crud.Options, router?: Router): Router.IMiddleware;
 
 export = crud;
--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 'use strict';
 
 const {Parser} = require('@easycrud/toolkits');
+const Router = require('koa-router');
+const db = require('./db');
 
-module.exports = function(opts) {
+module.exports = async function(opts, router) {
   opts = opts || {};
-  const {path} = opts;
-  let {tables} =opts;
+  const {path, dbConfigs} = opts;
+  if (!dbConfigs) {
+    throw new Error('Set at least one database connection config please.');
+  }
+
+  let {tables} = opts;
   if (path) {
     const parser = new Parser();
     parser.parse(path);
     tables = parser.tables;
   }
+
+  const dbConns = dbConfigs.map((conf) => db.connect(conf, conf.database));
+  await Promise.all(dbConns);
+
+  if (!router) {
+    router = new Router();
+  }
+
+  return router.routes();
 };
